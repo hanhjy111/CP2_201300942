66dfb449d9f8faeb7b26023f9758f9d98273468f
hanhjy111 <hanhjy111@naver.com>
2018-09-28 16:31:26 +0900

    third assignment

---
+++ ClassHomework3/Lab03/src/chocolatejargame/ChocolateJar.java
@@ -0,0 +1,29 @@
+package chocolatejargame;
+
+public class ChocolateJar implements Jar{
+
+	@Override
+	public int getItem() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void takeItem(int hand) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String status() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ ClassHomework3/Lab03/src/chocolatejargame/ChocolateJarMain.java
@@ -0,0 +1,10 @@
+package chocolatejargame;
+
+public class ChocolateJarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
